9149ae17e86885aa436c09e00e1c11ce6c1e05ef
zhanghong <hong.zhang@axinan.com>
2024-04-19 14:57:10 +0800

    feat: add test case

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,59 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
+/*
+ * @Author: Hong.Zhang
+ * @Date: 2024-04-18 17:25:35
+ * @Description:
+ */
+import invariant from 'invariant';
+
+export const treeifyArray = (arr: any[], propertyNames: string[]) => {
+  const result: Record<string, any> = {};
+  arr.forEach(item => {
+    let curNode = result;
+    for (let i = 0; i < propertyNames.length; i++) {
+      const isLast = i === propertyNames.length - 1;
+      const property = propertyNames[i];
+      const key = item[property];
+      invariant(
+        !!key && typeof key === 'string',
+        `the value corresponding to ${property} must be string`
+      );
+      if (!curNode[key]) {
+        curNode[key] = isLast ? [] : {};
+      }
+      if (isLast) {
+        curNode[key].push(item);
+      } else {
+        curNode = curNode[key];
+      }
+    }
+  });
+  return result;
+};
+
+export const treeifyArrayRecursive = (
+  arr: any[],
+  propertyNames: string[],
+  depth = 0
+) => {
+  if (depth >= propertyNames.length || arr.length === 0) {
+    return arr;
   }
-  return a + b;
+  const grouped: Record<string, any[]> = arr.reduce((acc, item) => {
+    const key = item[propertyNames[depth]];
+    invariant(
+      !!key && typeof key === 'string',
+      `the value corresponding to ${key} must be string`
+    );
+    if (!acc[key]) {
+      acc[key] = [];
+    }
+    acc[key].push(item);
+    return acc;
+  }, {});
+  const result: Record<string, any> = {};
+  Object.entries(grouped).forEach(
+    ([key, value]) =>
+      (result[key] = treeifyArrayRecursive(value, propertyNames, depth + 1))
+  );
+  return result;
 };
---
+++ test/index.test.ts
@@ -0,0 +1,70 @@
+/*
+ * @Author: Hong.Zhang
+ * @Date: 2024-04-19 11:37:47
+ * @Description:
+ */
+import { treeifyArray } from '../src';
+
+test('normal result', () => {
+  const items = [
+    { name: 'Alice', grade: 'grade-1', class: 'class-1' },
+    { name: 'Bob', grade: 'grade-1', class: 'class-2' },
+    { name: 'Charlie', grade: 'grade-2', class: 'class-3' },
+    { name: 'David', grade: 'grade-2', class: 'class-4' },
+  ];
+  const propertyNames = ['grade', 'class'];
+  const result = {
+    'grade-1': {
+      'class-1': [
+        {
+          name: 'Alice',
+          grade: 'grade-1',
+          class: 'class-1',
+        },
+      ],
+      'class-2': [
+        {
+          name: 'Bob',
+          grade: 'grade-1',
+          class: 'class-2',
+        },
+      ],
+    },
+    'grade-2': {
+      'class-3': [
+        {
+          name: 'Charlie',
+          grade: 'grade-2',
+          class: 'class-3',
+        },
+      ],
+      'class-4': [
+        {
+          name: 'David',
+          grade: 'grade-2',
+          class: 'class-4',
+        },
+      ],
+    },
+  };
+
+  expect(treeifyArray(items, propertyNames)).toStrictEqual(result);
+});
+
+test('value should not be null', () => {
+  const items = [
+    { name: 'Alice', age: undefined, gender: 'Male' },
+    { name: 'Bob', age: '11', class: 'Female' },
+  ];
+  const propertyNames = ['gender', 'age'];
+  expect(() => treeifyArray(items, propertyNames)).toThrow();
+});
+
+test('value should be string', () => {
+  const items = [
+    { name: 'Alice', age: {}, gender: 'Male' },
+    { name: 'Bob', age: '11', class: 'Female' },
+  ];
+  const propertyNames = ['gender', 'age'];
+  expect(() => treeifyArray(items, propertyNames)).toThrow();
+});
